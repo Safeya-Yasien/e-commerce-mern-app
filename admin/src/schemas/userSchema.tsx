8b9bc05908c30671d0fa63bfb648017543020b69
Safeya-Yasien <ssafeya5@gmail.com>
2025-11-26 22:35:27 +0200

    - update user schema by adding strict rules for gender - update user schema by adding password field

--- admin/src/schemas/userSchema.tsx
+++ admin/src/schemas/userSchema.tsx
@@ -3,12 +3,14 @@ import * as z from "zod";
 export const UserSchema = z.object({
   firstName: z.string().nonempty("First name is required").min(2, "Too Short"),
   lastName: z.string().nonempty("Last name is required").min(2, "Too Short"),
-  gender: z.string(),
+  gender: z.string().nonempty("Gender is required"),
   country: z.string(),
   phone: z.string().nonempty("Phone is required"),
   email: z.email().nonempty("Email is required"),
-
-  // lastUpdate: z.string(),
+  role: z.string().nonempty("Role is required"),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
 });
 
 export type IUserForm = z.infer<typeof UserSchema>;
